3bf37d571ab6b5fcc966ca1ba5387b7630cb0e20
Vũ Đức Hưng <hungvd18@gmail.com>
2026-07-27 22:27:19 +0700

    change url to connect with Server

--- SharedLab-Frontend-FULL-37-Screens-BUILD-FIXED-main/SharedLab-Frontend-FULL-37-Screens/src/environments/environment.development.ts
+++ SharedLab-Frontend-FULL-37-Screens-BUILD-FIXED-main/SharedLab-Frontend-FULL-37-Screens/src/environments/environment.development.ts
@@ -3,7 +3,7 @@ export const environment = {
   appName: 'Shared Lab Booking System',
   // Trong môi trường development, Angular gọi cùng origin /api.
   // Dev server sẽ proxy request sang backend HTTPS tại https://localhost:7073.
-  apiBaseUrl: 'https://sharedlabandequipmentbookingsystem.onrender.com/api',
+  apiBaseUrl: '/api',
   defaultLocale: 'vi',
   supportedLocales: ['vi', 'en'] as const,
 }
