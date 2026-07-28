ad2cc31ac4977e2ce1a21376c1f7a64cc9384924
ThanhPhong98 <dtp987234@gmail.com>
2026-07-28 16:01:29 +0700

    update enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   appName: 'Shared Lab Booking System',
-  apiBaseUrl: '/api',
+  apiBaseUrl: 'https://sharedlabandequipmentbookingsystem.onrender.com/api',
   defaultLocale: 'vi',
   supportedLocales: ['vi', 'en'] as const,
 }
